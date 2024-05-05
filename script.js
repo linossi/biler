f46c63237ba5d69367d2e01b0fc89d06d807f723
Linossi <145012929+linossi@users.noreply.github.com>
2024-05-05 17:18:03 +0200

    save

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('bilUtleieSkjema').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var bilType = document.getElementById('bilType').value;
+    var dager = parseInt(document.getElementById('dager').value);
+    var pris = beregnPris(bilType, dager);
+    document.getElementById('total').value = pris + ' kr';
+});
+
+function beregnPris(bilType, dager) {
+    var prisPerDag = 0;
+    switch (bilType) {
+        case 'standard':
+            prisPerDag = 500;
+            break;
+        case 'premium':
+            prisPerDag = 800;
+            break;
+        case 'luksus':
+            prisPerDag = 1200;
+            break;
+        default:
+            prisPerDag = 0;
+    }
+    return prisPerDag * dager;
+}
